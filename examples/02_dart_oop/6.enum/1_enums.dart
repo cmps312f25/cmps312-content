7d5f633db6f7673b557b597e853a58c8063a18bb
ae <erradi@live.com>
2025-09-07 09:48:19 +0300

    Update 1_enums.dart

--- examples/02_dart_oop/6.enum/1_enums.dart
+++ examples/02_dart_oop/6.enum/1_enums.dart
@@ -1,41 +1,34 @@
 enum Color { white, black }
+
 enum Weekday { sunday, monday, tuesday, wednesday, thursday }
+
 enum PlanetType { terrestrial, gas, ice }
+
 enum Weather { sunny, cloudy, rainy }
 
 void main() {
-  var today = "sunday";
   var day = Weekday.sunday;
-
-  switch(today) {
-
-  }
-
-  switch(day) {
+  switch (day) {
     case Weekday.sunday:
       print("Today is Sunday.");
     case Weekday.monday:
       print("Today is Monday.");
     case Weekday.tuesday:
       print("Today is Tuesday.");
-
     case Weekday.wednesday:
       print("Today is Wednesday.");
-      break;
     default:
       print("Today is Thursday.");
-      break;
-
   }
 
-
   Color c = Color.black;
-  Weekday d = Weekday.values[Weekday.friday.index];
+  Weekday d = Weekday.values[Weekday.thursday.index];
   Weather w = Weather.sunny;
-  
-  if (c == Color.black && d == Weekday.friday && w == Weather.sunny) {
+
+  if (c == Color.black && d == Weekday.thursday && w == Weather.sunny) {
     print("Let's go shopping!");
-  } if (w == Weather.rainy) {
+  }
+  if (w == Weather.rainy) {
     print("Stay at home!");
   } else {
     print("Wait for good deals! and good weather!");
